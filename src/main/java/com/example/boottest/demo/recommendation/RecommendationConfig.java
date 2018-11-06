ceb4f83b93c81acde9872f5314fa10f865a9236e
GuanSX <shanxu100@163.com>
2018-11-06 20:21:47 +0800

    windows

---
+++ src/main/java/com/example/boottest/demo/recommendation/RecommendationConfig.java
@@ -0,0 +1,24 @@
+package com.example.boottest.demo.recommendation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Guan
+ * @date Created on 2018/11/6
+ */
+@Configuration
+@Component
+public class RecommendationConfig {
+
+    @Value("${recommendation.dataset.path}")
+    private String datasetPath;
+
+
+    @Bean(name = "datasetPath")
+    public String getDatasetPath() {
+        return datasetPath;
+    }
+}
