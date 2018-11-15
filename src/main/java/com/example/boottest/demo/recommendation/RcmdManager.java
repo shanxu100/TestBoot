dfc37f1c273658426661fc30aab67ae0958cb7fb
GuanSX <shanxu100@163.com>
2018-11-15 20:47:42 +0800

    windows

--- src/main/java/com/example/boottest/demo/recommendation/RcmdManager.java
+++ src/main/java/com/example/boottest/demo/recommendation/RcmdManager.java
@@ -54,12 +54,14 @@ public class RcmdManager {
     public void loadDataModel() {
         try {
             System.out.println("开始加载DataModel...");
-            long time1 = System.currentTimeMillis();
+            //记录开始时间，计算耗时
+            long timestamp = System.currentTimeMillis();
+
             //准备数据 这里是电影评分数据
             File file = new File(datasetPath);
             //将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
             dataModel = new GroupLensDataModel(file);
-            System.out.println("加载DataModel耗时：" + (System.currentTimeMillis() - time1));
+            System.out.println("加载DataModel耗时：" + (System.currentTimeMillis() - timestamp));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -79,7 +81,7 @@ public class RcmdManager {
         if (dataModel == null) {
             return Collections.emptyList();
         }
-        long time1 = System.currentTimeMillis();
+        long timestamp = System.currentTimeMillis();
         //计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
         UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
         //计算最近邻域，邻居有两种算法，基于固定数量的邻居和基于相似度的邻居，这里使用基于固定数量的邻居
@@ -91,7 +93,8 @@ public class RcmdManager {
         List<RecommendedItem> recommendedItemList = recommender.recommend(userId, howMany);
 
         //打印推荐的结果
-        System.out.println("使用基于用户的协同过滤算法,为用户:" + userId + "推荐 " + howMany + " 个商品。耗时：" + (System.currentTimeMillis() - time1));
+        System.out.println("使用基于用户的协同过滤算法,为用户:" + userId + "推荐 " + howMany + " 个商品。" +
+                "耗时：" + (System.currentTimeMillis() - timestamp));
         for (RecommendedItem recommendedItem : recommendedItemList) {
             System.out.println(recommendedItem);
         }
@@ -109,6 +112,9 @@ public class RcmdManager {
         if (dataModel == null) {
             return -1;
         }
+
+        long timestamp = System.currentTimeMillis();
+
         //推荐评估，使用均方根
         //RecommenderEvaluator evaluator = new RMSRecommenderEvaluator();
         //推荐评估，使用平均差值
@@ -124,6 +130,8 @@ public class RcmdManager {
         // 用70%的数据用作训练，剩下的30%用来测试.最后得出的评估值越小，说明推荐结果越好
         double score = evaluator.evaluate(builder, null, dataModel, 0.7, 1.0);
 
+        System.out.println("模型评估，score=" + score + "耗时：" + (System.currentTimeMillis() - timestamp));
+
         return score;
     }
 
@@ -137,12 +145,15 @@ public class RcmdManager {
         if (dataModel == null) {
             return null;
         }
+        //记录开始时间，计算耗时
+        long timestamp = System.currentTimeMillis();
+
         RecommenderIRStatsEvaluator statsEvaluator = new GenericRecommenderIRStatsEvaluator();
         RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
             @Override
             public Recommender buildRecommender(DataModel model) throws TasteException {
                 UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-                UserNeighborhood neighborhood = new NearestNUserNeighborhood(40, similarity, model);
+                UserNeighborhood neighborhood = new NearestNUserNeighborhood(4, similarity, model);
                 return new GenericUserBasedRecommender(model, neighborhood, similarity);
             }
         };
@@ -150,8 +161,8 @@ public class RcmdManager {
         //使用评估器，并设定评估期的参数
         //4表示"precision and recall at 4"即相当于推荐top4，然后在top-4的推荐上计算准确率和召回率
         IRStatistics stats = statsEvaluator.evaluate(recommenderBuilder, null, dataModel, null, 4, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 1.0);
-        System.out.println(stats.getPrecision());
-        System.out.println(stats.getRecall());
+        System.out.println("准确率：" + stats.getPrecision() + "   召回率：" + stats.getRecall() + "  耗时："
+                + (System.currentTimeMillis() - timestamp) + " ms");
         return stats;
     }
 
